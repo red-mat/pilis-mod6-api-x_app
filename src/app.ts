d73070114d7fd4016016a9a340e2b2d53646d53c
red-mat <jesus.ing.rodriguez@gmail.com>
2023-08-04 19:41:11 -0300

    feat(app): add users route

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import { API_ROOT } from "./shared/environment";
-import { auth } from "./users/routes";
+import { auth, users } from "./users/routes";
 
 const ROOT_V1 = API_ROOT + "/v1";
 const app = express();
@@ -10,5 +10,6 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 app.use(ROOT_V1, auth);
+app.use(ROOT_V1, users);
 
 export default app;
