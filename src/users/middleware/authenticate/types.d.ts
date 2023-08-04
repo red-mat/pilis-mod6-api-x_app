0e2a2c6b1bcef7f38a64e72af17fab8ef2fe75c5
red-mat <jesus.ing.rodriguez@gmail.com>
2023-08-04 19:04:23 -0300

    fix(users): update type auth middleware

--- src/users/middleware/authenticate/types.d.ts
+++ src/users/middleware/authenticate/types.d.ts
@@ -1,6 +1,8 @@
 import { UserData } from "@/users/core/types";
 import { Request } from "express";
 
-export interface AuthRequest extends Request {
+interface IAuthRequest extends Request {
   user: UserData;
 }
+
+export type AuthRequest = IAuthRequest | any;
