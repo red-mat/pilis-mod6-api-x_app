3d13f30d50a55f128cf899fe5830ab91d362a666
red-mat <jesus.ing.rodriguez@gmail.com>
2023-08-04 19:35:29 -0300

    fix(app): update auth route

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import { API_ROOT } from "./shared/environment";
-import { authenticate } from "./users/controllers";
-import { users } from "./users/routes";
+import { auth } from "./users/routes";
 
 const ROOT_V1 = API_ROOT + "/v1";
 const app = express();
@@ -10,7 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.use(ROOT_V1, authenticate);
-app.use(ROOT_V1, users);
+app.use(ROOT_V1, auth);
 
 export default app;
