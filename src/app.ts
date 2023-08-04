5c5bcaac73a8cf3e008d15b7e11f831409a09585
red-mat <jesus.ing.rodriguez@gmail.com>
2023-08-03 23:51:34 -0300

    feat: update app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import { API_ROOT } from "./shared/environment";
+import { authenticate } from "./users/controllers";
+import morgan from "morgan";
 
 const ROOT_V1 = API_ROOT + "/v1";
 const app = express();
 
-app.use(ROOT_V1, (req, res) => {
-  return res.send("hola mundo");
-});
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use(ROOT_V1, authenticate);
 
 export default app;
