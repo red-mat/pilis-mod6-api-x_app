54ebd21de3fdc3f1ef73efda2a13e549508730fb
red-mat <jesus.ing.rodriguez@gmail.com>
2023-08-03 22:26:31 -0300

    feat(test): update app test

--- src/__test__/app.test.ts
+++ src/__test__/app.test.ts
@@ -3,7 +3,7 @@ import request from "supertest";
 
 describe("Testing app", () => {
   test("should response hello word", async () => {
-    const response = await request(app).get("/").send();
+    const response = await request(app).get("/api/v1").send();
 
     expect(response.status).toEqual(200);
     expect(response.text).toEqual("hola mundo");
