5bb0fa4c50efe79b70734e789877cecadadf4604
red-mat <jesus.ing.rodriguez@gmail.com>
2023-08-15 18:47:59 -0300

    feat(shared): add opent method storage

--- src/shared/storage/index.ts
+++ src/shared/storage/index.ts
@@ -2,6 +2,7 @@ import fs from "fs";
 import path from "path";
 
 type FilePath = string | null;
+type FIleBuffer = Buffer | null;
 const STORAGE_PATH = path.resolve("./", "storage");
 class Storage {
   private readonly path: string;
@@ -12,11 +13,30 @@ class Storage {
     fs.mkdirSync(STORAGE_PATH);
   }
 
+  static open(filePath: string): FIleBuffer {
+    try {
+      return fs.readFileSync(filePath);
+    } catch (error) {
+      return null;
+    }
+  }
+
   constructor(folder: string) {
     this.path = path.join(STORAGE_PATH, folder);
     const isPathFolder = fs.existsSync(this.path);
     if (isPathFolder) return;
     fs.mkdirSync(this.path);
+  }
+
+  open(name: string): FIleBuffer {
+    const filePath = path.join(this.path, name);
+
+    try {
+      return fs.readFileSync(filePath);
+    } catch (error) {
+      console.log((error as Error).message);
+      return null;
+    }
   }
 
   save(file: Buffer, name: string): FilePath {
