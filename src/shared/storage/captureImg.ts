e162a6b66cf8ab74c17037a75f67bf7395f9187f
red-mat <jesus.ing.rodriguez@gmail.com>
2023-08-18 20:46:23 -0300

    fix(shared): resolve captureImg storage middleware

--- src/shared/storage/captureImg.ts
+++ src/shared/storage/captureImg.ts
@@ -1,19 +1,22 @@
 import multer from "multer";
 
-const storage = multer.memoryStorage();
-const upload = multer({
-  storage: storage,
-  fileFilter: (req, file, cb) => {
-    if (!file.mimetype.startsWith("image/")) {
-      return cb(null, false);
-    }
+export default function captureImg(fieldName: string) {
+  const storage = multer.memoryStorage();
+  const upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+      if (!file.mimetype.startsWith("image/")) {
+        return cb(null, false);
+      }
 
-    const maxSize = 5 * 1024 * 1024;
-    if (file.size > maxSize) {
-      return cb(null, false);
-    }
+      const maxSize = 5 * 1024 * 1024;
+      if (file.size > maxSize) {
+        return cb(null, false);
+      }
 
-    cb(null, true);
-  },
-});
-export default upload.single;
+      cb(null, true);
+    },
+  });
+
+  return upload.single(fieldName);
+}
