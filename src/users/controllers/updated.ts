f76925a0dee6924b49bdb33d679a1162c41bc510
red-mat <jesus.ing.rodriguez@gmail.com>
2023-08-18 21:04:46 -0300

    feat(users): add avatar updater controler

--- src/users/controllers/updated.ts
+++ src/users/controllers/updated.ts
@@ -2,16 +2,31 @@ import { Response } from "express";
 import { User, UserEntity } from "../core";
 import { UserModel } from "../core/types";
 import { AuthRequest } from "../middleware/authenticate";
+import mime from "mime";
+import { Storage } from "@/shared/storage";
 
 const message = (message: string) => ({ message });
 const getEntity = async (id: number) =>
   await UserEntity.findOneBy({ id, isDeleted: false });
+const updateAvatar = (
+  file: Express.Multer.File,
+  userId: number
+): string | null => {
+  const storage = new Storage("avatar");
+  const format = mime.extension(file.mimetype);
+  const buffer = file.buffer;
+  const name = `${userId}.${format}`;
+
+  return storage.reWrite(buffer, name);
+};
 
 async function updated(req: AuthRequest, res: Response) {
+  const img = req.file;
+
   const { id } = req.user;
   const data: Partial<UserModel> = req.body;
 
-  if (!data.username && !data.password)
+  if (!data.username && !data.password && !img)
     return res.status(400).send(message("params not valid"));
 
   let userEntity;
@@ -27,9 +42,11 @@ async function updated(req: AuthRequest, res: Response) {
 
   if (data.username) user.username = data.username;
   if (data.password) user.password = data.password;
+  if (img) user.avatar = updateAvatar(req.file, id);
 
   userEntity.username = user.username;
   userEntity.password = user.password;
+  userEntity.avatar = user.avatar;
 
   try {
     await userEntity.save();
--- src/users/routes/users.ts
+++ src/users/routes/users.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { authenticate } from "../middleware/authenticate";
 import { avatar, updated } from "../controllers";
+import { captureImg } from "@/shared/storage";
 
 const PATH = "/users";
 const route = Router();
 
-route.put(PATH, authenticate, updated);
+route.put(PATH, authenticate, captureImg("avatar"), updated);
 route.get("/users/avatar/:id", avatar);
 
 export default route;
