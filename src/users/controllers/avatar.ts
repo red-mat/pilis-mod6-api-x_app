bc8420c93edb7c3551ac89a49603f5769360fa3f
red-mat <jesus.ing.rodriguez@gmail.com>
2023-08-21 18:18:05 -0300

    fix(users): update avatar controller
    se establecio una serie de acciones en caso de que el archivo almacenado
    en la base de datos no exista.

--- src/users/controllers/avatar.ts
+++ src/users/controllers/avatar.ts
@@ -1,11 +1,21 @@
 import { Request, Response } from "express";
 import { UserEntity } from "../core";
+import { Storage } from "@/shared/storage";
+
+async function resolveAvatarNotFound(user: UserEntity) {
+  user.avatar = null;
+  user.save();
+}
 
 async function avatar(req: Request, res: Response) {
   const { id } = req.params;
   const user = await UserEntity.findOneBy({ id: Number(id) });
   if (!user) return res.sendStatus(400);
   if (!user.avatar) return res.sendStatus(400);
+  if (!Storage.exists(user.avatar)) {
+    resolveAvatarNotFound(user)
+    return res.sendStatus(500);
+  }
 
   return res.sendFile(user.avatar);
 }
