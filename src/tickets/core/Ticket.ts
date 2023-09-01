9a59934382e861e4808dc9b947c07f3e9f5d515e
red-mat <jesus.ing.rodriguez@gmail.com>
2023-09-01 19:23:32 -0300

    feat(ticket): update core

---
+++ src/tickets/core/Ticket.ts
@@ -0,0 +1,115 @@
+import { Order } from "@/orders/core/OrderEntity";
+import { Status } from "@/orders/core/constans";
+import { MoreThan } from "typeorm";
+import CodeMachine from "./CodeMachine";
+import TicketEntity from "./TicketEntity";
+
+const EXPIRED_TIME = 4 * 60 * 60 * 1000;
+const LENGTH_CODE = 4;
+
+class Ticket {
+  private readonly entity: TicketEntity;
+
+  private constructor(entity: TicketEntity) {
+    this.entity = entity;
+  }
+
+  private static async getOrder(id: string): Promise<Order | null> {
+    const currentDate = new Date();
+    const fourHoursAgo = new Date(currentDate.getTime() - EXPIRED_TIME);
+
+    const where = {
+      id,
+      status: Status.FINISHED,
+      updatedAt: MoreThan(fourHoursAgo),
+    };
+
+    try {
+      const order = await Order.findOneBy(where);
+      if (!order) return null;
+
+      return null;
+    } catch (error) {
+      console.log(error);
+
+      return null;
+    }
+  }
+
+  private static async getCodes(): Promise<string[]> {
+    const currentDate = new Date();
+    const fourHoursAgo = new Date(currentDate.getTime() - EXPIRED_TIME);
+
+    const where = {
+      status: Status.FINISHED,
+      updatedAt: MoreThan(fourHoursAgo),
+    };
+
+    const orders = await Order.findBy(where);
+    if (!orders) return [];
+
+    const ordersId = orders.map((o) => o.id);
+
+    const tickets = await TicketEntity.findBy({ order: ordersId });
+    if (!tickets) return [];
+
+    const codes = tickets.map((T) => T.code);
+    return codes;
+  }
+
+  private static async generateCode(): Promise<string> {
+    const codes = await this.getCodes();
+    const generator = new CodeMachine(codes);
+    return generator.generateCode(LENGTH_CODE);
+  }
+
+  static async Generate(orderId: string): Promise<Ticket | null> {
+    const order = await this.getOrder(orderId);
+    if (!order) return null;
+
+    const ticket = new TicketEntity();
+    ticket.order = order;
+    ticket.code = await this.generateCode();
+    await ticket.save();
+
+    return new Ticket(ticket);
+  }
+
+  static async Find(id: string): Promise<Ticket | null> {
+    const ticket = await TicketEntity.findOneBy({ id });
+    if (!ticket) return null;
+
+    return new Ticket(ticket);
+  }
+
+  isExpired(): boolean {
+    const order = this.entity.order as Order;
+    const currentTime = new Date().getTime();
+    const orderUpdatedAt = order.updatedAt.getTime();
+
+    const timeSinceLastUpdate = currentTime - orderUpdatedAt;
+    const isExpired = timeSinceLastUpdate >= EXPIRED_TIME;
+
+    return isExpired;
+  }
+
+  async updateCode(): Promise<void> {
+    if (this.isExpired()) return;
+
+    this.entity.code = await Ticket.generateCode();
+    await this.entity.save();
+  }
+
+  dto() {
+    const id = this.entity.id;
+    const isExpired = this.isExpired();
+    const orderId = (this.entity.order as Order).id;
+    const createAt = this.entity.createdAt;
+    const isDelivered = this.entity.isDelivered;
+    const code = this.entity.code;
+
+    return { id, createAt, isDelivered, isExpired, code, orderId };
+  }
+}
+
+export default Ticket;
