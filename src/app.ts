6002a78bc707a7af90b1464a6b02704727791e32
red-mat <jesus.ing.rodriguez@gmail.com>
2023-09-01 23:39:12 -0300

    feat(app): update routes

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,8 @@ import morgan from "morgan";
 import { API_ROOT } from "./shared/environment";
 import productRoute from "./products/routes";
 import { auth, users } from "./users/routes";
-import orderRoute from "./orders/routes"
+import orderRoute from "./orders/routes";
+import ticketRoute from "./tickets/routes";
 
 const ROOT_V1 = API_ROOT + "/v1";
 const app = express();
@@ -13,7 +14,7 @@ app.use(express.json());
 
 app.use(ROOT_V1, auth);
 app.use(ROOT_V1, users);
-app.use(ROOT_V1, productRoute)
-app.use(ROOT_V1, orderRoute)
-
+app.use(ROOT_V1, productRoute);
+app.use(ROOT_V1, orderRoute);
+app.use(ROOT_V1, ticketRoute);
 export default app;
