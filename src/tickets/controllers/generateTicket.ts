ceda98e960d515c0cbebfdec47792818f86e9700
red-mat <jesus.ing.rodriguez@gmail.com>
2023-09-04 23:27:07 -0300

    feat(tickets): update generateTicket controller
    add body message

--- src/tickets/controllers/generateTicket.ts
+++ src/tickets/controllers/generateTicket.ts
@@ -1,12 +1,16 @@
 import { Request, Response } from "express";
 import { Ticket } from "../core";
 
+function body(message: string) {
+  return { message };
+}
+
 async function generateTicket(req: Request, res: Response) {
   const { orderId } = req.params;
-  if (!orderId) return res.sendStatus(500);
+  if (!orderId) return res.status(400).send(body("send ticket id"));
 
   const ticket = await Ticket.Generate(orderId);
-  if (!ticket) return res.sendStatus(400);
+  if (!ticket) return res.status(400).send(body("ticket not found"));
 
   return res.send(ticket.dto());
 }
