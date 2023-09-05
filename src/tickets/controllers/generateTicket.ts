768648976f24dafdb54331f99dcbedf13c7a9b10
red-mat <jesus.ing.rodriguez@gmail.com>
2023-09-05 15:05:21 -0300

    fix(tickets): update generateTIcket controller
    body message with orderId is null

--- src/tickets/controllers/generateTicket.ts
+++ src/tickets/controllers/generateTicket.ts
@@ -7,7 +7,7 @@ function body(message: string) {
 
 async function generateTicket(req: Request, res: Response) {
   const { orderId } = req.params;
-  if (!orderId) return res.status(400).send(body("send ticket id"));
+  if (!orderId) return res.status(400).send(body("send order id"));
 
   const ticket = await Ticket.Generate(orderId);
   if (!ticket) return res.status(400).send(body("ticket not found"));
