05f49e5396e215a58ba59cff146c884d056835a2
red-mat <jesus.ing.rodriguez@gmail.com>
2023-09-05 16:06:05 -0300

    feat(tickets): add refresh controller

--- src/tickets/controllers/index.ts
+++ src/tickets/controllers/index.ts
@@ -1,3 +1,4 @@
 export { default as deliver } from "./deliver";
 export { default as generateTicket } from "./generateTicket";
 export { default as getTickets } from "./getTickets";
+export { default as refresh } from "./refresh";
