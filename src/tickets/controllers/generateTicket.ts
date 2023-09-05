38588535ec08ddf5ddb2e6bafeeef24190b05d98
red-mat <jesus.ing.rodriguez@gmail.com>
2023-09-05 15:16:26 -0300

    feat(tickets): update generateTicket controller
    update error status and message

--- src/tickets/controllers/generateTicket.ts
+++ src/tickets/controllers/generateTicket.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { Ticket } from "../core";
+import { Order } from "@/orders/core/OrderEntity";
 
 function body(message: string) {
   return { message };
@@ -9,8 +10,11 @@ async function generateTicket(req: Request, res: Response) {
   const { orderId } = req.params;
   if (!orderId) return res.status(400).send(body("send order id"));
 
+  const order = await Order.findOneBy({ id: orderId });
+  if (!order) return res.status(404).send(body("order not found, invalid id"));
+
   const ticket = await Ticket.Generate(orderId);
-  if (!ticket) return res.status(400).send(body("ticket not found"));
+  if (!ticket) return res.status(403).send(body("Order is Expired"));
 
   return res.send(ticket.dto());
 }
