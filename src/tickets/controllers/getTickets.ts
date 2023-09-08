4c899a66a371021b1a91ffc467a83a513dc95705
red-mat <jesus.ing.rodriguez@gmail.com>
2023-09-08 18:04:19 -0300

    feat(tickets): add detail on getTickets controller

--- src/tickets/controllers/getTickets.ts
+++ src/tickets/controllers/getTickets.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
 import { Ticket, TicketEntity } from "../core";
 
+const RELATIONS = ["order", "order.orderDetail"];
+
 async function getTickets(req: Request, res: Response) {
-  const ticketEntities = await TicketEntity.find({ relations: ["order"] });
+  const ticketEntities = await TicketEntity.find({ relations: RELATIONS });
   if (ticketEntities.length === 0) return [];
 
   const tickets = ticketEntities.map((t) => new Ticket(t));
