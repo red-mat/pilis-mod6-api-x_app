223743c179f220484394d47954b61dd55659296c
red-mat <jesus.ing.rodriguez@gmail.com>
2023-09-12 18:23:14 -0300

    feat(app): add cors

--- src/app.ts
+++ src/app.ts
@@ -5,15 +5,16 @@ import productRoute from "./products/routes";
 import { auth, users } from "./users/routes";
 import orderRoute from "./orders/routes";
 import ticketRoute from "./tickets/routes";
-import path from 'path'
-
+import path from "path";
+import cors from "cors";
 
 const ROOT_V1 = API_ROOT + "/v1";
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
-app.use('/public', express.static(path.join(__dirname, '../storage')));//capture image
+app.use("/public", express.static(path.join(__dirname, "../storage"))); //capture image
 
 app.use(ROOT_V1, auth);
 app.use(ROOT_V1, users);
