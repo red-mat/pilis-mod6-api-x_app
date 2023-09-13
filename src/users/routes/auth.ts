7fc0525584c487656bac5cb4f78aeed0085a1724
red-mat <jesus.ing.rodriguez@gmail.com>
2023-09-12 21:28:24 -0300

    fix(users): resolve auth route

--- src/users/routes/auth.ts
+++ src/users/routes/auth.ts
@@ -6,7 +6,7 @@ import refresh from "../controllers/refresh";
 const PATH = "/auth";
 
 const route = Router();
-route.get(PATH, authenticate);
+route.post(PATH, authenticate);
 route.put(PATH, refreshMiddle, refresh);
 
 export default route;
