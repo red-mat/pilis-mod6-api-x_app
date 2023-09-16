ab11dc7a46a1723ef07abbdeb1237f9ce7cc7530
red-mat <jesus.ing.rodriguez@gmail.com>
2023-09-15 22:49:27 -0300

    feat(orders): public get order by id

--- src/orders/routes/index.ts
+++ src/orders/routes/index.ts
@@ -1,11 +1,11 @@
-import { Router } from "express"
-import { get, create, update } from "../controller/orderController"
+import { Router } from "express";
+import { get, create, update } from "../controller/orderController";
 import { authenticate } from "@/users/middleware/authenticate";
 
 const router: Router = Router();
 
 router.get("/order", authenticate, get);
-router.get("/order/:orderId", authenticate, get);
+router.get("/order/:orderId", get);
 router.post("/order", create);
 router.put("/order/:orderId", update);
 
